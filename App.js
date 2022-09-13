d230ef72c12bd18eb031c5bdbea40b685569926a
Jalil M. López <jalilmanuel25@gmail.com>
2022-09-13 17:56:37 -0500

    Cursos

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  SafeAreaView,
+  StatusBar,
+  Button,
+  Component,
+} from "react-native";
+
+import NavBar, { NavButton, NavButtonText, NavTitle } from 'react-native-nav'
 
 export default function App() {
+  const [hidden] = useState(false);
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        animated={true}
+        backgroundColor="#61dafb"
+        hidden={hidden}
+      />
+      
+      
+      <View style={styles.container}>
+        <Text style={styles.text}>Inicio de sesión</Text>
+        <TextInput 
+          editable 
+          maxLength={40}           
+          placeholder={'Correo electrónico'}
+          style={styles.input}
+        />
+        <TextInput 
+          editable 
+          maxLength={40}           
+          placeholder={'Contraseña'}
+          style={styles.input}
+        />
+        <Button title='Iniciar Sesión'
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +52,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 55,
+  },
+  input: {
+    width: 250,
+    height: 44,
+    padding: 10,
+    marginTop: 20,
+    marginBottom: 10,
+    backgroundColor: '#e8e8e8',
   },
 });
